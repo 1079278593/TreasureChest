c1ea5d75904ecb79bd702f9de2c9eb8b7c5ae746
xiao ming <1079278593@qq.com>
2020-02-25 14:45:57 +0800

    增加server mock，但是最近疫情导致外围基本无法访问。

---
+++ TreasureChestDemo/Demo/ServerMock/ServerMockCtl.h
@@ -0,0 +1,17 @@
+//
+//  ServerMockCtl.h
+//  TreasureChest
+//
+//  Created by xiao ming on 2020/2/25.
+//  Copyright © 2020 xiao ming. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ServerMockCtl : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TreasureChestDemo/Demo/ServerMock/ServerMockCtl.m
@@ -0,0 +1,32 @@
+//
+//  ServerMockCtl.m
+//  TreasureChest
+//
+//  Created by xiao ming on 2020/2/25.
+//  Copyright © 2020 xiao ming. All rights reserved.
+//
+
+#import "ServerMockCtl.h"
+
+@interface ServerMockCtl ()
+
+@end
+
+@implementation ServerMockCtl
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
