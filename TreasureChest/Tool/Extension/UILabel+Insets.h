d423a1646556caeb1576307144efe23ff3a698ac
xiao ming <1079278593@qq.com>
2020-04-03 16:30:48 +0800

    增加UILabel的边距category

---
+++ TreasureChest/Tool/Extension/UILabel+Insets.h
@@ -0,0 +1,19 @@
+//
+//  UILabel+Insets.h
+//  TreasureChest
+//
+//  Created by xiao ming on 2020/4/3.
+//  Copyright © 2019 xiao ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UILabel (Insets)
+
+@property (nonatomic, assign) UIEdgeInsets contentInsets;
+
+@end
+
+NS_ASSUME_NONNULL_END
