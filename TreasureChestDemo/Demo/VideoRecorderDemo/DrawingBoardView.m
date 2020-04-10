1af65cadec912d5b7e034534961592f1f07d7a5c
xiao ming <1079278593@qq.com>
2020-04-10 15:38:34 +0800

    增加画线条的代码

--- TreasureChestDemo/Demo/VideoRecorderDemo/DrawingBoardView.m
+++ TreasureChestDemo/Demo/VideoRecorderDemo/DrawingBoardView.m
@@ -7,15 +7,94 @@
 //
 
 #import "DrawingBoardView.h"
+@interface DrawingBoardView (){
+    CGPoint location,previousLocation;
+    CGPoint previousMidPoint;
+    CGFloat brushWidth;
+    NSString *brushTextImageName;
+    UIColor *lineColor;
+    
+    BOOL firstTouch;
+}
+
+@property (nonatomic, strong) UIBezierPath *renderPath;
+@property (nonatomic, strong) UIBezierPath *currentPath;
+@property (nonatomic, assign) CGPoint ctlPoint;
+@property (nonatomic, assign) CGFloat prePathLength;
+
+@end
 
 @implementation DrawingBoardView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)init {
+    if(self == [super init]){
+        [self initView];
+        self.backgroundColor = [UIColor whiteColor];
+    }
+    return self;
 }
-*/
+
+- (void)drawRect:(CGRect)rect{
+
+    [[UIColor redColor] set];
+
+    UIBezierPath* path = self.renderPath;
+
+    path.lineWidth     = 5.f;
+    path.lineCapStyle  = kCGLineCapRound;
+    path.lineJoinStyle = kCGLineCapRound;
+
+    [path stroke];
+}
+
+#pragma mark - touchesEvent
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+
+    UITouch *touch = [[event touchesForView:self] anyObject];
+    previousLocation = [touch locationInView:self];
+    
+    self.currentPath = [UIBezierPath bezierPath];
+    [self.currentPath moveToPoint:previousLocation];
+    self.renderPath = [UIBezierPath bezierPathWithCGPath:self.currentPath.CGPath];
+    firstTouch = YES;
+}
+
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [[event touchesForView:self] anyObject];
+    location = [touch locationInView:self];
+    
+    if (self.renderPath == nil) {
+        self.renderPath = [UIBezierPath bezierPath];
+        [self.renderPath moveToPoint:self.currentPath.currentPoint];
+    }
+    if (CGPointEqualToPoint(self.ctlPoint, CGPointZero)) {
+        self.ctlPoint = location;
+        CGPoint midPoint = CGPointMake((self.currentPath.currentPoint.x + location.x) * 0.5, (self.currentPath.currentPoint.y + location.y) * 0.5);
+        [self.currentPath addLineToPoint:midPoint];
+        [self.renderPath addLineToPoint:midPoint];
+    } else {
+        CGPoint midPoint = CGPointMake((location.x + self.ctlPoint.x) * 0.5, (location.y + self.ctlPoint.y) * 0.5);
+        [self.currentPath addQuadCurveToPoint:midPoint controlPoint:self.ctlPoint];
+        self.ctlPoint = location;
+        [self.renderPath addQuadCurveToPoint:midPoint controlPoint:self.ctlPoint];
+        firstTouch = NO;
+    }
+    
+    [self setNeedsDisplay];//强制更新
+}
+
+// Handles the end of a touch event when the touch is a tap.
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self touchesMoved:touches withEvent:event];
+    self.ctlPoint = CGPointZero;
+}
+
+#pragma mark - < init >
+- (void)initView {
+    
+}
+
 
 @end
--- TreasureChestDemo/Demo/VideoRecorderDemo/VideoRecorderCtl.m
+++ TreasureChestDemo/Demo/VideoRecorderDemo/VideoRecorderCtl.m
@@ -20,17 +20,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self initView];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - < event >
+- (void)startRecordBtnEvent:(UIButton *)button {
+    if (!button.selected) {
+        //开始录制
+    }else {
+        //结束录制
+    }
+    button.selected = !button.selected;
 }
-*/
 
+#pragma mark - < init >
+- (void)initView {
+    
+    [self.view addSubview:self.startRecordBtn];
+    [self.startRecordBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.bottom.equalTo(self.view).offset(-10);
+        make.width.equalTo(@80);
+        make.height.equalTo(@(40));
+    }];
+    
+    self.drawView = [[DrawingBoardView alloc]init];
+    self.drawView.layer.borderWidth = 1;
+    [self.view addSubview:self.drawView];
+    [self.drawView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view).offset(10);
+        make.right.equalTo(self.view).offset(-10);
+        make.bottom.equalTo(self.startRecordBtn.mas_top).offset(-5);
+        make.top.equalTo(@64);
+    }];
+}
+
+- (UIButton *)startRecordBtn {
+    if (_startRecordBtn == nil) {
+        _startRecordBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_startRecordBtn setTitle:@"开始录制" forState:UIControlStateNormal];
+        [_startRecordBtn setTitle:@"结束录制" forState:UIControlStateSelected];
+        [_startRecordBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_startRecordBtn addTarget:self action:@selector(startRecordBtnEvent:) forControlEvents:UIControlEventTouchUpInside];
+        _startRecordBtn.titleLabel.font = [UIFont systemFontOfSize:15];
+        [self.view addSubview:_startRecordBtn];
+        
+    }
+    return _startRecordBtn;
+}
 @end
