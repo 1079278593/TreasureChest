06ecb1153f6db72ff4a4caaa5b7e7b7be80e78ef
xiao ming <1079278593@qq.com>
2020-04-21 11:48:31 +0800

    1.调整目录结构；2.开始第二部分课程：动画和绘制

---
+++ TreasureChestSwiftDemo/TreasureChestSwiftDemo/Demo/Drawing_Animation/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  TreasureChestSwiftDemo
+//
+//  Created by xiao ming on 2020/4/21.
+//  Copyright © 2020 xiao ming. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
