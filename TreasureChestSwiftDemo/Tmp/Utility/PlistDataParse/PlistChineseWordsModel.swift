84eca1b02559eb59fb5041e0356d8a3303e4df98
xiao ming <1079278593@qq.com>
2020-05-07 18:44:23 +0800

    增加词库model

---
+++ TreasureChestSwiftDemo/Tmp/Utility/PlistDataParse/PlistChineseWordsModel.swift
@@ -0,0 +1,15 @@
+//
+//  PlistChineseWordsModel.swift
+//  TreasureChestSwiftDemo
+//
+//  Created by xiao ming on 2020/5/7.
+//  Copyright © 2020 xiao ming. All rights reserved.
+//
+
+import UIKit
+import HandyJSON
+
+struct PlistChineseWordsModel: HandyJSON {
+    var words = ""
+    var frequency = ""
+}
