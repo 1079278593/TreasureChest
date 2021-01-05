86924b1cdbd2e8c089fda1976e004fc76c30576b
ming <1079278593@qq.com>
2021-01-05 11:11:42 +0800

    像素操作方法

---
+++ TreasureChest/Tool/PixelsOperator/PixelsOperator.h
@@ -0,0 +1,17 @@
+//
+//  PixelsOperator.h
+//  TreasureChest
+//
+//  Created by jf on 2021/1/5.
+//  Copyright © 2021 xiao ming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PixelsOperator : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
