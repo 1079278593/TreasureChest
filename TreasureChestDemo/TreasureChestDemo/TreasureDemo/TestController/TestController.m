0065df901a4b0aff8b50632ac5daff4604f727ea
ming <1079278593@qq.com>
2021-02-22 14:44:12 +0800

    完善

--- TreasureChestDemo/TreasureChestDemo/TreasureDemo/TestController/TestController.m
+++ TreasureChestDemo/TreasureChestDemo/TreasureDemo/TestController/TestController.m
@@ -14,6 +14,9 @@
 #import "FaceMaskRenderer.h"
 #import "FileManager.h"
 
+#define RADIANS_TO_DEGREES(radians) ((radians) * (180.0 / M_PI))
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
 @interface TestController ()
 
 @property(nonatomic, strong)UIButton *button;
@@ -83,18 +86,8 @@
 //    NSString *path = [self getDatas].firstObject;
     
     [self.lottieView setAnimationProgress:progress];
-    
-//    self.lottieView.layer.presentationLayer drawInContext:<#(nonnull CGContextRef)#>
-//    [self.lottieView play];
-    
-    CGSize size = CGSizeMake(640, 480);
-    UIGraphicsBeginImageContext(size);
-    [self.lottieView.layer renderInContext: UIGraphicsGetCurrentContext()];
-    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
-    self.imgView.image = viewImage;
-    UIGraphicsEndImageContext();
-    
 //    self.imgView.image = [self pixelBufferFromLayer:self.lottieView.layer];
+    self.imgView.image = [self pixelBufferLeftLandscapeFromLayer:self.lottieView.layer];
 }
 
 #pragma mark - < init view >
@@ -108,14 +101,20 @@
     LOTComposition *composition = [LOTComposition animationWithFilePath:path];
     self.lottieView = [[LOTAnimationView alloc]initWithModel:composition inBundle:nil];
 //    [self.view addSubview:self.lottieView];
-    self.lottieView.frame = CGRectMake(0, 80, 640, 480);
+    self.lottieView.frame = CGRectMake(0, 80, 480, 640);
     self.lottieView.userInteractionEnabled = false;
     
     self.imgView = [[UIImageView alloc]init];
     self.imgView.userInteractionEnabled = false;
     [self.view addSubview:_imgView];
-    _imgView.frame = CGRectMake(10, 90, 150, 150);
+    _imgView.frame = CGRectMake(10, 90, 150, 150/(640/480.0));
     _imgView.layer.borderWidth = 1;
+    
+    LOTAnimationView *tmpLottieView = [[LOTAnimationView alloc]initWithModel:composition inBundle:nil];
+    [self.view addSubview:tmpLottieView];
+    tmpLottieView.frame = CGRectMake(10, _imgView.bottom + 10, _imgView.width, _imgView.height);
+    tmpLottieView.userInteractionEnabled = false;
+    [tmpLottieView setAnimationProgress:0.2];
 }
 
 - (NSArray *)getDatas {
@@ -125,7 +124,7 @@
     return lotties;
 }
 
-- (CVPixelBufferRef)pixelBufferFromLayer:(CALayer *)layer {
+- (UIImage *)pixelBufferFromLayer:(CALayer *)layer {
     CGSize frameSize = CGSizeMake((int)layer.bounds.size.width,(int)layer.bounds.size.height);
     NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES],
                              kCVPixelBufferCGImageCompatibilityKey,
@@ -149,15 +148,53 @@
     CGContextTranslateCTM(context, 0, frameSize.height);
     CGContextScaleCTM(context, 1.0, -1.0);
     
-    //调试查看
-//    [layer renderInContext:context];
-//    UIImage *viewImage = [UIImage imageWithCGImage:CGBitmapContextCreateImage(context)];
-//    return viewImage;
+    //调试:查看
+    [layer renderInContext:context];
+    UIImage *viewImage = [UIImage imageWithCGImage:CGBitmapContextCreateImage(context)];
+    return viewImage;
     
     CGColorSpaceRelease(rgbColorSpace);
     CGContextRelease(context);
     CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
-    return pxbuffer;
+//    return pxbuffer;
 }
 
+//传入的layer比例为：480*640，要变成640*480
+- (UIImage *)pixelBufferLeftLandscapeFromLayer:(CALayer *)layer {
+    CGSize frameSize = CGSizeMake((int)layer.bounds.size.height,(int)layer.bounds.size.width);
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES],
+                             kCVPixelBufferCGImageCompatibilityKey,
+                             [NSNumber numberWithBool:YES],
+                             kCVPixelBufferCGBitmapContextCompatibilityKey,nil];
+    CVPixelBufferRef pxbuffer = NULL;
+    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault, frameSize.width, frameSize.height,kCVPixelFormatType_32BGRA, (__bridge CFDictionaryRef)options, &pxbuffer);
+    NSParameterAssert(status == kCVReturnSuccess && pxbuffer != NULL);
+    CVPixelBufferLockBaseAddress(pxbuffer, 0);
+    void *pxdata = CVPixelBufferGetBaseAddress(pxbuffer);
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    CGContextRef context = CGBitmapContextCreate(pxdata, frameSize.width, frameSize.height,8,
+                                                 CVPixelBufferGetBytesPerRow(pxbuffer),rgbColorSpace,(CGBitmapInfo)kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+
+    /**
+     UIKit － y轴向下
+     Core Graphics(Quartz) － y轴向上
+     OpenGL ES － y轴向上
+     */
+    CGContextTranslateCTM(context, 0, frameSize.width);
+    CGContextScaleCTM(context, 1.0, -1.0);//翻转后，原点变为左上角(原来为左下角),saveState
+    
+    CGContextRotateCTM(context, DEGREES_TO_RADIANS(-90));//旋转后，x和y轴颠倒
+    CGContextTranslateCTM(context, -frameSize.width, frameSize.width*0);
+    
+    //调试查看
+    [layer renderInContext:context];
+    UIImage *viewImage = [UIImage imageWithCGImage:CGBitmapContextCreateImage(context)];
+    return viewImage;
+    
+    CGColorSpaceRelease(rgbColorSpace);
+    CGContextRelease(context);
+    CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
+//    return pxbuffer;
+}
 @end
