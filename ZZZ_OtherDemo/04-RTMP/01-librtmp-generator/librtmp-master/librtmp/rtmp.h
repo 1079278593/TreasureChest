1dcd7ddf4f8d2f47077e983b918465098c7322de
ming <mhhuang@imvt.com>
2021-07-08 09:02:33 +0800

    gnu库的netdb相关代码

--- ZZZ_OtherDemo/04-RTMP/01-librtmp-generator/librtmp-master/librtmp/rtmp.h
+++ ZZZ_OtherDemo/04-RTMP/01-librtmp-generator/librtmp-master/librtmp/rtmp.h
@@ -29,7 +29,7 @@
 #define CRYPTO
 #endif
 
-#include "netdb.h"
+#include <netdb.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -41,16 +41,16 @@ extern "C"
 {
 #endif
 
-#define RTMP_LIB_VERSION	0x020300	/* 2.3 */
+#define RTMP_LIB_VERSION    0x020300    /* 2.3 */
 
-#define RTMP_FEATURE_HTTP	0x01
-#define RTMP_FEATURE_ENC	0x02
-#define RTMP_FEATURE_SSL	0x04
-#define RTMP_FEATURE_MFP	0x08	/* not yet supported */
-#define RTMP_FEATURE_WRITE	0x10	/* publish, not play */
-#define RTMP_FEATURE_HTTP2	0x20	/* server-side rtmpt */
+#define RTMP_FEATURE_HTTP    0x01
+#define RTMP_FEATURE_ENC    0x02
+#define RTMP_FEATURE_SSL    0x04
+#define RTMP_FEATURE_MFP    0x08    /* not yet supported */
+#define RTMP_FEATURE_WRITE    0x10    /* publish, not play */
+#define RTMP_FEATURE_HTTP2    0x20    /* server-side rtmpt */
 
-#define RTMP_PROTOCOL_UNDEFINED	-1
+#define RTMP_PROTOCOL_UNDEFINED    -1
 #define RTMP_PROTOCOL_RTMP      0
 #define RTMP_PROTOCOL_RTMPE     RTMP_FEATURE_ENC
 #define RTMP_PROTOCOL_RTMPT     RTMP_FEATURE_HTTP
@@ -59,12 +59,12 @@ extern "C"
 #define RTMP_PROTOCOL_RTMPTS    (RTMP_FEATURE_HTTP|RTMP_FEATURE_SSL)
 #define RTMP_PROTOCOL_RTMFP     RTMP_FEATURE_MFP
 
-#define RTMP_DEFAULT_CHUNKSIZE	128
+#define RTMP_DEFAULT_CHUNKSIZE    128
 
 /* needs to fit largest number of bytes recv() may return */
 #define RTMP_BUFFER_CACHE_SIZE (16*1024)
 
-#define	RTMP_CHANNELS	65600
+#define    RTMP_CHANNELS    65600
 
   extern const char RTMPProtocolStringsLower[][7];
   extern const AVal RTMP_DefaultFlashVer;
@@ -115,10 +115,10 @@ extern "C"
   {
     uint8_t m_headerType;
     uint8_t m_packetType;
-    uint8_t m_hasAbsTimestamp;	/* timestamp absolute or relative? */
+    uint8_t m_hasAbsTimestamp;    /* timestamp absolute or relative? */
     int m_nChannel;
-    uint32_t m_nTimeStamp;	/* timestamp */
-    int32_t m_nInfoField2;	/* last 4 bytes in a long header */
+    uint32_t m_nTimeStamp;    /* timestamp */
+    int32_t m_nInfoField2;    /* last 4 bytes in a long header */
     uint32_t m_nBodySize;
     uint32_t m_nBytesRead;
     RTMPChunk *m_chunk;
@@ -128,9 +128,9 @@ extern "C"
   typedef struct RTMPSockBuf
   {
     int sb_socket;
-    int sb_size;		/* number of unprocessed bytes in buffer */
-    char *sb_start;		/* pointer into sb_pBuffer of next byte to process */
-    char sb_buf[RTMP_BUFFER_CACHE_SIZE];	/* data read from socket */
+    int sb_size;        /* number of unprocessed bytes in buffer */
+    char *sb_start;        /* pointer into sb_pBuffer of next byte to process */
+    char sb_buf[RTMP_BUFFER_CACHE_SIZE];    /* data read from socket */
     int sb_timedout;
     void *sb_ssl;
   } RTMPSockBuf;
@@ -140,15 +140,15 @@ extern "C"
   int RTMPPacket_Alloc(RTMPPacket *p, uint32_t nSize);
   void RTMPPacket_Free(RTMPPacket *p);
 
-#define RTMPPacket_IsReady(a)	((a)->m_nBytesRead == (a)->m_nBodySize)
+#define RTMPPacket_IsReady(a)    ((a)->m_nBytesRead == (a)->m_nBodySize)
 
   typedef struct RTMP_LNK
   {
     AVal hostname;
     AVal sockshost;
 
-    AVal playpath0;	/* parsed from URL */
-    AVal playpath;	/* passed in explicitly */
+    AVal playpath0;    /* parsed from URL */
+    AVal playpath;    /* passed in explicitly */
     AVal tcUrl;
     AVal swfUrl;
     AVal pageUrl;
@@ -166,28 +166,28 @@ extern "C"
     int seekTime;
     int stopTime;
 
-#define RTMP_LF_AUTH	0x0001	/* using auth param */
-#define RTMP_LF_LIVE	0x0002	/* stream is live */
-#define RTMP_LF_SWFV	0x0004	/* do SWF verification */
-#define RTMP_LF_PLST	0x0008	/* send playlist before play */
-#define RTMP_LF_BUFX	0x0010	/* toggle stream on BufferEmpty msg */
-#define RTMP_LF_FTCU	0x0020	/* free tcUrl on close */
-#define RTMP_LF_FAPU	0x0040	/* free app on close */
+#define RTMP_LF_AUTH    0x0001    /* using auth param */
+#define RTMP_LF_LIVE    0x0002    /* stream is live */
+#define RTMP_LF_SWFV    0x0004    /* do SWF verification */
+#define RTMP_LF_PLST    0x0008    /* send playlist before play */
+#define RTMP_LF_BUFX    0x0010    /* toggle stream on BufferEmpty msg */
+#define RTMP_LF_FTCU    0x0020    /* free tcUrl on close */
+#define RTMP_LF_FAPU    0x0040    /* free app on close */
     int lFlags;
 
     int swfAge;
 
     int protocol;
-    int timeout;		/* connection timeout in seconds */
+    int timeout;        /* connection timeout in seconds */
 
-    int pFlags;			/* unused, but kept to avoid breaking ABI */
+    int pFlags;            /* unused, but kept to avoid breaking ABI */
 
     unsigned short socksport;
     unsigned short port;
 
 #ifdef CRYPTO
-#define RTMP_SWF_HASHLEN	32
-    void *dh;			/* for encryption */
+#define RTMP_SWF_HASHLEN    32
+    void *dh;            /* for encryption */
     void *rc4keyIn;
     void *rc4keyOut;
 
@@ -206,17 +206,17 @@ extern "C"
     uint32_t timestamp;
     uint8_t dataType;
     uint8_t flags;
-#define RTMP_READ_HEADER	0x01
-#define RTMP_READ_RESUME	0x02
-#define RTMP_READ_NO_IGNORE	0x04
-#define RTMP_READ_GOTKF		0x08
-#define RTMP_READ_GOTFLVK	0x10
-#define RTMP_READ_SEEKING	0x20
+#define RTMP_READ_HEADER    0x01
+#define RTMP_READ_RESUME    0x02
+#define RTMP_READ_NO_IGNORE    0x04
+#define RTMP_READ_GOTKF        0x08
+#define RTMP_READ_GOTFLVK    0x10
+#define RTMP_READ_SEEKING    0x20
     int8_t status;
-#define RTMP_READ_COMPLETE	-3
-#define RTMP_READ_ERROR	-2
-#define RTMP_READ_EOF	-1
-#define RTMP_READ_IGNORE	0
+#define RTMP_READ_COMPLETE    -3
+#define RTMP_READ_ERROR    -2
+#define RTMP_READ_EOF    -1
+#define RTMP_READ_IGNORE    0
 
     /* if bResume == TRUE */
     uint8_t initialFrameType;
@@ -243,7 +243,7 @@ extern "C"
     int m_nBytesIn;
     int m_nBytesInSent;
     int m_nBufferMS;
-    int m_stream_id;		/* returned in _result from createStream */
+    int m_stream_id;        /* returned in _result from createStream */
     int m_mediaChannel;
     uint32_t m_mediaStamp;
     uint32_t m_pauseStamp;
@@ -257,21 +257,21 @@ extern "C"
 
     int m_numInvokes;
     int m_numCalls;
-    RTMP_METHOD *m_methodCalls;	/* remote method calls queue */
+    RTMP_METHOD *m_methodCalls;    /* remote method calls queue */
 
     int m_channelsAllocatedIn;
     int m_channelsAllocatedOut;
     RTMPPacket **m_vecChannelsIn;
     RTMPPacket **m_vecChannelsOut;
-    int *m_channelTimestamp;	/* abs timestamp of last packet */
+    int *m_channelTimestamp;    /* abs timestamp of last packet */
 
-    double m_fAudioCodecs;	/* audioCodecs for the connect packet */
-    double m_fVideoCodecs;	/* videoCodecs for the connect packet */
-    double m_fEncoding;		/* AMF0 or AMF3 */
+    double m_fAudioCodecs;    /* audioCodecs for the connect packet */
+    double m_fVideoCodecs;    /* videoCodecs for the connect packet */
+    double m_fEncoding;        /* AMF0 or AMF3 */
 
-    double m_fDuration;		/* duration of stream in seconds */
+    double m_fDuration;        /* duration of stream in seconds */
 
-    int m_msgCounter;		/* RTMPT stuff */
+    int m_msgCounter;        /* RTMPT stuff */
     int m_polling;
     int m_resplen;
     int m_unackd;
@@ -282,14 +282,14 @@ extern "C"
     RTMPSockBuf m_sb;
     RTMP_LNK Link;
 
-    struct gaicb *reqs;
+    struct addrinfo *reqs;
     int resolve_dns;
     int connect;
     struct sockaddr_in service;
   } RTMP;
 
   int RTMP_ParseURL(const char *url, int *protocol, AVal *host,
-		     unsigned int *port, AVal *playpath, AVal *app);
+             unsigned int *port, AVal *playpath, AVal *app);
 
   void RTMP_ParsePlaypath(AVal *in, AVal *out);
   void RTMP_SetBufferMS(RTMP *r, int size);
@@ -298,22 +298,22 @@ extern "C"
   int RTMP_SetOpt(RTMP *r, const AVal *opt, AVal *arg);
   int RTMP_SetupURL(RTMP *r, char *url);
   void RTMP_SetupStream(RTMP *r, int protocol,
-			AVal *hostname,
-			unsigned int port,
-			AVal *sockshost,
-			AVal *playpath,
-			AVal *tcUrl,
-			AVal *swfUrl,
-			AVal *pageUrl,
-			AVal *app,
-			AVal *auth,
-			AVal *swfSHA256Hash,
-			uint32_t swfSize,
-			AVal *flashVer,
-			AVal *subscribepath,
-			AVal *usherToken,
-			int dStart,
-			int dStop, int bLiveStream, long int timeout);
+            AVal *hostname,
+            unsigned int port,
+            AVal *sockshost,
+            AVal *playpath,
+            AVal *tcUrl,
+            AVal *swfUrl,
+            AVal *pageUrl,
+            AVal *app,
+            AVal *auth,
+            AVal *swfSHA256Hash,
+            uint32_t swfSize,
+            AVal *flashVer,
+            AVal *subscribepath,
+            AVal *usherToken,
+            int dStart,
+            int dStop, int bLiveStream, long int timeout);
 
   int RTMP_ResolveDNSCancel(RTMP *r);
   int RTMP_ResolveDNS(RTMP *r);
@@ -349,10 +349,10 @@ extern "C"
   void RTMP_TLS_FreeServerContext(void *ctx);
 
   int RTMP_LibVersion(void);
-  void RTMP_UserInterrupt(void);	/* user typed Ctrl-C */
+  void RTMP_UserInterrupt(void);    /* user typed Ctrl-C */
 
   int RTMP_SendCtrl(RTMP *r, short nType, unsigned int nObject,
-		     unsigned int nTime);
+             unsigned int nTime);
 
   /* caller probably doesn't know current timestamp, should
    * just use RTMP_Pause instead
@@ -361,7 +361,7 @@ extern "C"
   int RTMP_Pause(RTMP *r, int DoPause);
 
   int RTMP_FindFirstMatchingProperty(AMFObject *obj, const AVal *name,
-				      AMFObjectProperty * p);
+                      AMFObjectProperty * p);
 
   int RTMPSockBuf_Fill(RTMPSockBuf *sb);
   int RTMPSockBuf_Send(RTMPSockBuf *sb, const char *buf, int len);
@@ -377,7 +377,18 @@ extern "C"
 
 /* hashswf.c */
   int RTMP_HashSWF(const char *url, unsigned int *size, unsigned char *hash,
-		   int age);
+           int age);
+
+//struct gaicb
+//{
+//  const char *ar_name;        /* Name to look up.  */
+//  const char *ar_service;    /* Service name.  */
+//  const struct addrinfo *ar_request; /* Additional request specification.  */
+//  struct addrinfo *ar_result;    /* Pointer to result.  */
+//  /* The following are internal elements.  */
+//  int __return;
+//  int __glibc_reserved[5];
+//};
 
 #ifdef __cplusplus
 };
